Separate training and prediction tasks. Create TomoPhantoms. Use correct scaling for ssim and psnr

cryoS2Sdrop/cryoS2Sdrop/model.py
@@ -183,10 +183,12 @@ class Denoising_3DUNet(pl.LightningModule):
             bernoulliBatch_subtomo = self.batch2bernoulliBatch(bernoulli_subtomo)
             bernoulliBatch_pred = self.batch2bernoulliBatch(pred)
             bernoulliBatch_gt_subtomo = self.batch2bernoulliBatch(gt_subtomo)
-            baseline_ssim = self.ssim_monitoring(bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo)
-            baseline_psnr = self.psnr_monitoring(bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo)
-            monitor_ssim = self.ssim_monitoring(bernoulliBatch_pred, bernoulliBatch_gt_subtomo)
-            monitor_psnr = self.psnr_monitoring(bernoulliBatch_pred, bernoulliBatch_gt_subtomo)
+            baseline_ssim, baseline_psnr = self.ssim_psnr_monitoring(
+                bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo
+            )
+            monitor_ssim, monitor_psnr = self.ssim_psnr_monitoring(
+                bernoulliBatch_pred, bernoulliBatch_gt_subtomo
+            )
 
             self.log(
                 "ssim/baseline",
@@ -234,28 +236,30 @@ class Denoising_3DUNet(pl.LightningModule):
     def batch2bernoulliBatch(self, subtomo):
         return torch.split(subtomo, self.n_bernoulli_samples)
 
-    def ssim_monitoring(self, bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo):
-        monitor = 0
-        for bBatch_subtomo, bBatch_gt in zip(bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo):
-            _monitor = ssim(bBatch_subtomo.mean((0)), bBatch_gt.mean((0)))
-            monitor += _monitor
+    def ssim_psnr_monitoring(self, bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo):
+        ssim_monitor = 0
+        psnr_monitor = 0
+        for bBatch_subtomo, bBatch_gt in zip(
+            bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo
+        ):
+            # we first normalize the images
+            X = bBatch_subtomo.mean(0)
+            X = (X - X.min() + 1e-4) / (X.max() - X.min() + 1e-4)
+            Y = bBatch_gt.mean(0)
+            Y = (Y - Y.min() + 1e-4) / (Y.max() - Y.min() + 1e-4)
+
+            _ssim, _psnr = float(ssim(X, Y, data_range=1)), float(
+                peak_signal_noise_ratio(X, Y, data_range=1)
+            )
+            ssim_monitor += _ssim
+            psnr_monitor += _psnr
 
         # take the mean wrt batch
-        return monitor/len(bernoulliBatch_gt_subtomo)
+        ssim_monitor = ssim_monitor / len(bernoulliBatch_gt_subtomo)
+        psnr_monitor = psnr_monitor / len(bernoulliBatch_gt_subtomo)
 
-    def psnr_monitoring(self, bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo):
-        monitor = 0
-        for bBatch_subtomo, bBatch_gt in zip(bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo):
-            i, j = bBatch_subtomo.mean((0)), bBatch_gt.mean((0)) # 1 Channel, 3D images [C, S, S, S]
-            # data is standardized, we don't expect to see any value further than 10 std from the origin
-            data_range = 18 
-            if j.abs().max() > 9:
-                raise ValueError('Found input values for ground truth further than 9 std away from the origin.')
-            _monitor = peak_signal_noise_ratio(i, j, data_range)
-            monitor += _monitor
+        return ssim_monitor, psnr_monitor
 
-        # take the mean wrt batch
-        return monitor/len(bernoulliBatch_gt_subtomo)
 
 class Denoising_3DUNet_v2(Denoising_3DUNet):
     def training_step(self, batch):
@@ -276,10 +280,12 @@ class Denoising_3DUNet_v2(Denoising_3DUNet):
             bernoulliBatch_subtomo = self.batch2bernoulliBatch(bernoulli_subtomo)
             bernoulliBatch_pred = self.batch2bernoulliBatch(pred)
             bernoulliBatch_gt_subtomo = self.batch2bernoulliBatch(gt_subtomo)
-            baseline_ssim = self.ssim_monitoring(bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo)
-            baseline_psnr = self.psnr_monitoring(bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo)
-            monitor_ssim = self.ssim_monitoring(bernoulliBatch_pred, bernoulliBatch_gt_subtomo)
-            monitor_psnr = self.psnr_monitoring(bernoulliBatch_pred, bernoulliBatch_gt_subtomo)
+            baseline_ssim, baseline_psnr = self.ssim_psnr_monitoring(
+                bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo
+            )
+            monitor_ssim, monitor_psnr = self.ssim_psnr_monitoring(
+                bernoulliBatch_pred, bernoulliBatch_gt_subtomo
+            )
 
             self.log(
                 "ssim/baseline",
@@ -326,6 +332,7 @@ class Denoising_3DUNet_v2(Denoising_3DUNet):
 
 
 ############################### 2D ################################################################
+
 
 class Denoising_2DUNet(pl.LightningModule):
     def __init__(self, loss_fn, lr, n_features, p, n_bernoulli_samples):
@@ -493,8 +500,12 @@ class Denoising_2DUNet(pl.LightningModule):
         if gt_subtomo is not None:
             bernoulliBatch_subtomo = self.batch2bernoulliBatch(bernoulli_subtomo)
             bernoulliBatch_gt_subtomo = self.batch2bernoulliBatch(gt_subtomo)
-            monitor_ssim = self.ssim_monitoring(bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo)
-            monitor_psnr = self.psnr_monitoring(bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo)
+            monitor_ssim = self.ssim_monitoring(
+                bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo
+            )
+            monitor_psnr = self.psnr_monitoring(
+                bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo
+            )
 
             self.log(
                 "hp/ssim",
@@ -526,26 +537,34 @@ class Denoising_2DUNet(pl.LightningModule):
 
     def ssim_monitoring(self, bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo):
         monitor = 0
-        for bBatch_subtomo, bBatch_gt in zip(bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo):
+        for bBatch_subtomo, bBatch_gt in zip(
+            bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo
+        ):
             _monitor = ssim(bBatch_subtomo.mean((0)), bBatch_gt.mean((0)))
             monitor += _monitor
 
         # take the mean wrt batch
-        return monitor/len(bernoulliBatch_gt_subtomo)
+        return monitor / len(bernoulliBatch_gt_subtomo)
 
     def psnr_monitoring(self, bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo):
         monitor = 0
-        for bBatch_subtomo, bBatch_gt in zip(bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo):
-            i, j = bBatch_subtomo.mean((0)), bBatch_gt.mean((0)) # 1 Channel, 3D images [C, S, S, S]
+        for bBatch_subtomo, bBatch_gt in zip(
+            bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo
+        ):
+            i, j = bBatch_subtomo.mean((0)), bBatch_gt.mean(
+                (0)
+            )  # 1 Channel, 3D images [C, S, S, S]
             # data is standardized, we don't expect to see any value further than 10 std from the origin
-            data_range = 18 
+            data_range = 18
             if j.abs().max() > 9:
-                raise ValueError('Found input values for ground truth further than 9 std away from the origin.')
+                raise ValueError(
+                    "Found input values for ground truth further than 9 std away from the origin."
+                )
             _monitor = peak_signal_noise_ratio(i, j, data_range)
             monitor += _monitor
 
         # take the mean wrt batch
-        return monitor/len(bernoulliBatch_gt_subtomo)
+        return monitor / len(bernoulliBatch_gt_subtomo)
 
 
 class Denoising_2DUNet_v2(Denoising_2DUNet):
@@ -566,8 +585,12 @@ class Denoising_2DUNet_v2(Denoising_2DUNet):
         if gt_subtomo is not None:
             bernoulliBatch_subtomo = self.batch2bernoulliBatch(bernoulli_subtomo)
             bernoulliBatch_gt_subtomo = self.batch2bernoulliBatch(gt_subtomo)
-            monitor_ssim = self.ssim_monitoring(bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo)
-            monitor_psnr = self.psnr_monitoring(bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo)
+            monitor_ssim = self.ssim_monitoring(
+                bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo
+            )
+            monitor_psnr = self.psnr_monitoring(
+                bernoulliBatch_subtomo, bernoulliBatch_gt_subtomo
+            )
 
             self.log(
                 "hp/ssim",

cryoS2Sdrop/cryoS2Sdrop/predict.py
@@ -1,3 +1,4 @@
+from cmath import log
 from cryoS2Sdrop.model import Denoising_3DUNet, Denoising_3DUNet_v2
 from cryoS2Sdrop.trainer import aggregate_bernoulliSamples
 import torch
@@ -15,6 +16,8 @@ def aux_forward(model, subtomo):
 
 def load_model(logdir, DataParallel=False):
     "Returns loaded model from checkpoint and hyperparameters"
+    if not logdir.endswith("/"):
+        logdir = logdir + "/"
 
     with open(glob(logdir + "hparams.yaml")[0]) as f:
         hparams = yaml.load(f, Loader=yaml.BaseLoader)
@@ -23,21 +26,23 @@ def load_model(logdir, DataParallel=False):
     with open(logdir + "hparams.yaml") as f:
         s = f.readlines()
         try:
-            dataloader = [x for x in s if 'Dataloader' in x][0]
-            dataset = dataloader.split('.')[-1].replace('\n', '').replace('\'', '').strip()
+            dataloader = [x for x in s if "Dataloader" in x][0]
+            dataset = (
+                dataloader.split(".")[-1].replace("\n", "").replace("'", "").strip()
+            )
         except:
-            dataset = 'Unknown'
+            dataset = "Unknown"
 
-    hparams['dataset'] = dataset
+    hparams["dataset"] = dataset
 
-    p = float(hparams['p']) # dropout (zeroing) probability
-    n_features = int(hparams['n_features'])
-    n_bernoulli_samples = int(hparams['n_bernoulli_samples'])
+    p = float(hparams["p"])  # dropout (zeroing) probability
+    n_features = int(hparams["n_features"])
+    n_bernoulli_samples = int(hparams["n_bernoulli_samples"])
 
-    if dataset in ['singleCET_dataset']:
+    if dataset in ["singleCET_dataset"]:
         model = Denoising_3DUNet(None, 0, n_features, p, n_bernoulli_samples)
-        
-    if dataset in ['singleCET_FourierDataset', 'singleCET_ProjectedDataset']:
+
+    if dataset in ["singleCET_FourierDataset", "singleCET_ProjectedDataset"]:
         model = Denoising_3DUNet_v2(None, 0, n_features, p, n_bernoulli_samples)
 
     ckpt_file = glob(logdir + "checkpoints/*.ckpt")
@@ -56,6 +61,7 @@ def collate_fn(batch):
     batch = [list(filter(lambda x: x is not None, b)) for b in batch]
     return torch.utils.data.dataloader.default_collate(batch)
 
+
 def predict_full_tomogram(singleCET_dataset, model, N=100):
 
     tomo_shape = singleCET_dataset.tomo_shape
@@ -65,15 +71,17 @@ def predict_full_tomogram(singleCET_dataset, model, N=100):
     count_tensor = torch.zeros(tomo_shape)  # for averaging overlapping patches
 
     for idx, p0 in enumerate(tqdm(singleCET_dataset.grid)):
-        zmin, ymin, xmin = np.array(p0)-subtomo_length//2
-        zmax, ymax, xmax = np.array(p0)+subtomo_length//2
-        subtomo = singleCET_dataset[idx][0]  # shape: [M, C:=1, S, S, S] or  [C:=1, S, S, S]
-        if len(subtomo.shape)==4: # the projected dataset yields this type
-            subtomo = subtomo.unsqueeze(0) # equivalent to having "1 Bernoulli sample"
+        zmin, ymin, xmin = np.array(p0) - subtomo_length // 2
+        zmax, ymax, xmax = np.array(p0) + subtomo_length // 2
+        subtomo = singleCET_dataset[idx][
+            0
+        ]  # shape: [M, C:=1, S, S, S] or  [C:=1, S, S, S]
+        if len(subtomo.shape) == 4:  # the projected dataset yields this type
+            subtomo = subtomo.unsqueeze(0)  # equivalent to having "1 Bernoulli sample"
         # we want to average each patch over N Bernoulli samples, typically N >> M
         M = len(subtomo)
         # effective number of samples
-        n_times = N//M + 1
+        n_times = N // M + 1
         pred = torch.cat([model(subtomo).detach().cpu() for i in range(n_times)])
         # take mean over predictions and reduce channels
         pred = pred.mean(0).squeeze()

scripts/train_n2vUnet.py
@@ -19,6 +19,26 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 PARENT_PATH = setup.PARENT_PATH
 
+
+def standardize(X: torch.tensor):
+    mean = X.mean()
+    std = X.std()
+
+    new_X = (X - mean) / std
+
+    return new_X
+
+
+def scale(X):
+    scaled = (X - X.min()) / (X.max() - X.min())
+    return scaled
+
+
+def clip(X, low=0.005, high=0.995):
+    # works with tensors =)
+    return np.clip(X, np.quantile(X, low), np.quantile(X, high))
+
+
 ###################### Input data definition ###################
 
 # cet_path = os.path.join(PARENT_PATH, 'data/raw_cryo-ET/tomo02.mrc')
@@ -30,6 +50,7 @@ cet_path = os.path.join(
 )
 
 gt_cet_path = None
+clip_values = False
 
 name = tomo_name
 
@@ -43,6 +64,10 @@ datagen = N2V_DataGenerator()
 # The function will return a list of images (numpy arrays).
 # In the 'dims' parameter we specify the order of dimensions in the image files we are reading.
 imgs = datagen.load_imgs([cet_path], dims="ZYX")
+# TODO check if standardization affects deconvolution, review order of operations.
+if clip_values:
+    imgs[0][0, ..., 0] = clip(imgs[0][0, ..., 0])
+imgs[0][0, ..., 0] = standardize(imgs[0][0, ..., 0])
 
 deconv_kwargs = {
     "angpix": 14,
@@ -156,6 +181,6 @@ if use_deconv_data:
     filename = cet_path.split("/")[-1].replace(".mrc", "_deconv_n2vDenoised")
 else:
     filename = cet_path.split("/")[-1].replace(".mrc", "_n2vDenoised")
-filename = os.path.join(PARENT_PATH, "data/S2SDenoising/denoised/%s.mrc" % (filename))
+filename = os.path.join(basedir, "%s.mrc" % (filename))
 
 write_array(pred, filename)