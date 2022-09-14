b06384ddcbe78f4039e5b14242c12b53526fc595
Ubuntu <ubuntu@fastfaraday-56eb2.denbi.local>
2022-09-14 08:08:47 +0000

    Use 1 Bernoulli sample per prediction to reach maximum independence of the network predictions. Add script to predict separately from training procedure.

--- scripts/run_predict_denoisingUnet.py
+++ scripts/run_predict_denoisingUnet.py
@@ -3,15 +3,34 @@ import json
 
 
 ################### SETUP ############################################
-experiment_name = "fourierWeightedBernoulliMask_comparison"
-tomo_name = "tomo02_dummy"
+experiment_name = "realBernoulli_dropoutLevel_comparison"
+# tomogram_list = [
+#     "shrec2021_model4_dummy",
+#     "shrec2021_model2_dummy"
+# ]
+
+tomogram_list = [
+    # "tomoPhantom_model8_noisyGaussPoissL",
+    # "tomoPhantom_model8_noisyGaussPoissM",
+    # "tomoPhantom_model8_noisyGaussPoissH",
+    # "tomoPhantom_model14_noisyGaussPoissL",
+    # "tomoPhantom_model14_noisyGaussPoissM",
+    # "tomoPhantom_model14_noisyGaussPoissH",
+    "tomoPhantom_model16_noisyGaussPoissL",
+    # "tomoPhantom_model16_noisyGaussPoissM",
+    # "tomoPhantom_model16_noisyGaussPoissH",
+]
+
 experiment_args = {
     # corresponding version of the model we want to use for prediction
-    "e2": {"version": "version_1", "n_bernoulli_samples": 12}
+    # "e0": {"version": "version_0", "n_bernoulli_samples_prediction": 1, "resample_patch_each_iter":True},
+    "e1": {"version": "version_1", "n_bernoulli_samples_prediction": 1, "resample_patch_each_iter":True},
+    # "e2": {"version": "version_2", "n_bernoulli_samples_prediction": 1, "resample_patch_each_iter":True},
+    # "e3": {"version": "version_3", "n_bernoulli_samples_prediction": 1, "resample_patch_each_iter":True}
 }
 ######################################################################
 
-default_args = {"tomo_name": None, "n_bernoulli_samples": 20, "version": None}
+default_args = {"tomo_name": None, "n_bernoulli_samples_prediction": 20, "version": None, "resample_patch_each_iter":True}
 
 
 def main(experiment_name, args):
@@ -21,16 +40,16 @@ def main(experiment_name, args):
 
 
 if __name__ == "__main__":
+    for tomo_name in tomogram_list:
+        for exp in experiment_args:
+            args = default_args.copy()
+            args["tomo_name"] = tomo_name
+            # the new args is the dictionary of the experiment arguments
+            new_args = experiment_args[exp]
 
-    for exp in experiment_args:
-        args = default_args.copy()
-        args["tomo_name"] = tomo_name
-        # the new args is the dictionary of the experiment arguments
-        new_args = experiment_args[exp]
+            # rewrite arguments for given experiment
+            for arg in new_args:
+                args[arg] = new_args[arg]
 
-        # rewrite arguments for given experiment
-        for arg in new_args:
-            args[arg] = new_args[arg]
-
-        # run code
-        main(experiment_name, args)
+            # run code
+            main(experiment_name, args)
